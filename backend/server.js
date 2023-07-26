d3b960c0164f85367c3ec767f6f4b970f0d7967d
Nabil Khan <nabilkhan@Nabils-MacBook-Pro.local>
2023-07-26 06:42:54 +0100

    state management

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js'
+import connectDB from './config/db.js';
+import { notFound , errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
 
@@ -10,12 +15,8 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, ()=> console.log(port));
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,76 +1,79 @@
-import {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios';
-
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = () => {
 
-  const [product, setProduct] = useState({})
   const {id:productId} = useParams()
+  const {data: product, isLoading, error} = useGetProductDetailsQuery(productId);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    }
+    return (
+    <>
+      <Link className='btn btn-light my-3' to="/">
+        Go Back
+      </Link>
 
-    fetchProduct();
-    
-  }, [productId])
-  
-
-  return <>
-      <Link className='btn btn-light my-3' to="/">Go Back</Link>
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Description: {product.description}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+      <Message variant='danger'>
+        {error?.data?.message || error.error }
+      </Message>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
             <ListGroup variant='flush'>
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>{product.countInStock > 0 ? 'In stock' : 'Out of Stock'}</strong>
-                  </Col>
-                </Row>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className='btn-block' type='buttton' disabled={product.countInStock === 0}>Add to Cart</Button>
+                Price: ${product.price}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      <strong>{product.countInStock > 0 ? 'In stock' : 'Out of Stock'}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button className='btn-block' type='buttton' disabled={product.countInStock === 0}>Add to Cart</Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
       </Row>
+      )}
+
     </>
-}
+
+)}
 
 export default ProductScreen
